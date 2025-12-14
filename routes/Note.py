7386ef95e97afb8363ff62b6ba0ec05bf11eefc5
Ayush Mahakud <rishimahakud3@gmail.com>
2025-12-14 12:23:59 +0530

    project done

--- routes/Note.py
+++ routes/Note.py
@@ -56,6 +56,71 @@ def notes():
     
     return jsonify(new_notes.to_dict()), 201
 
+
+@notes_blueprint.route("/<int:note_id>",methods=["PUT"])
+def update_data(note_id):
+   data = request.get_json()
+
+   if not data:
+      return jsonify({"error":"no data provided"}),400
+   
+   note = Notetable.query.get(note_id)
+
+   if not note:
+      return jsonify({"error":"no data found"}),400
+   
+   if "title" in data:
+      note.title=data["title"]
+
+   if "content" in data:
+     note.content=data["content"]
+
+   if "tags" in data:
+     note.tags=data["tags"]
+
+   db.session.commit()
+
+   return jsonify({
+        "id": note.id,
+        "title": note.title,
+        "content": note.content,
+        "tags": note.tags.split(",") if note.tags else []
+    }), 200
     
 
-    
+
+@notes_blueprint.route("/<int:note_id>",methods=["DELETE"])
+def delete_data(note_id):
+   note = Notetable.query.get(note_id)
+
+   if not note:
+      return jsonify({"error":"no data found"}),400
+   
+   db.session.delete(note)
+   db.session.commit()
+
+   return jsonify({"messege":"Data deleted succecfully"}),200
+
+
+
+@notes_blueprint.route("/",methods=["GET"])
+def search_data():
+   tag = request.args.get("tag") 
+
+   if tag:
+      notes = Notetable.query.filter(
+         Notetable.tags.like(f"{tag}")
+      )
+
+   
+   result= []
+   for note in notes:
+        result.append({
+            "id": note.id,
+            "title": note.title,
+            "content": note.content,
+            "tags": note.tags,
+            "created_at": note.created_at,
+            "updated_at": note.updated_at
+        })
+   return jsonify(result), 200
